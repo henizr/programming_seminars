814781c72ef2dd0b62af79ea2ec50c6ec5f0e074
Harry <andr.nitgrafika@yandex.ru>
2023-01-14 23:51:16 +0300

    Added Task04

---
+++ Task04/Program.cs
@@ -0,0 +1,18 @@
+﻿int max = 0;
+Console.WriteLine("Введите первое число: ");
+int numOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое число: ");
+int numTwo = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое число: ");
+int numThree = Convert.ToInt32(Console.ReadLine());
+
+max = numOne;
+
+if (numTwo > max){
+    max = numTwo;
+}
+if(numThree > max){
+    max = numThree;
+}
+
+Console.Write($"Максимальное число: {max}");
