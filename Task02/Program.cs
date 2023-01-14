087f2ab7273dc37db1811e73cba3ad6b801f5505
Harry <andr.nitgrafika@yandex.ru>
2023-01-14 23:41:10 +0300

    Added Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число: ");
+int numOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numTwo = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Максимальное число: ");
+if (numOne > numTwo)
+{
+    Console.WriteLine(numOne);
+}
+else{
+    Console.WriteLine(numTwo);
+}
