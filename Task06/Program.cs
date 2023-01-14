c1de798885aa5dc9ce8ff5d9c09776103029066b
Harry <andr.nitgrafika@yandex.ru>
2023-01-14 23:57:48 +0300

    Added Task06

---
+++ Task06/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0){
+    Console.WriteLine("Число чётное");
+}
+else{
+    Console.WriteLine("Число не чётное");
+}
