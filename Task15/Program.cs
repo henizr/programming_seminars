ba80854b6ed1e30258143df7ffdfda92075c3385
Harry <andr.nitgrafika@yandex.ru>
2023-01-18 15:57:22 +0300

    Added Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,16 @@
+﻿bool isDayOff(int dayNum)
+{
+    return dayNum == 6 || dayNum == 7;
+}
+
+Console.Write("Введите номер дня: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if(isDayOff(day))
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
