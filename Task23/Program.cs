8a780f52d8cd7a77423db214e219be2a6e61ac67
Harry <andr.nitgrafika@yandex.ru>
2023-01-22 21:20:23 +0300

    Added Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 1)
+{
+    Console.WriteLine("Некорректный ввод");
+}
+else
+{
+    GetCubeTable(num);
+}
+
+void GetCubeTable(int number)
+{
+    int i = 1;
+    while(i<=number)
+    {
+        Console.WriteLine($"{i} -> {i*i*i}");
+        i++;
+    }
+}
