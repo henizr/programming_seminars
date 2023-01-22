50f0c6e80a02104d9a09a04604e6aaea1b84fa7c
Harry <andr.nitgrafika@yandex.ru>
2023-01-22 20:42:16 +0300

    Added Task19

---
+++ Task19/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num < 10000 || num > 99999)
+{
+    Console.WriteLine("Число не пятизначное");
+}
+else
+{
+ if(IsPalindrom(num))
+ {
+    Console.WriteLine("Да");
+ }
+ else
+ {
+    Console.WriteLine("Нет");
+ }
+}
+
+
+bool IsPalindrom(int number)
+{
+    int firstNum = number / 1000;
+    int secondNum = number % 100;
+    int flippedSecondNum = (secondNum / 10) + (secondNum % 10) * 10;
+
+    if (firstNum == flippedSecondNum){
+        return true;
+    }
+    return false;
+}
+
