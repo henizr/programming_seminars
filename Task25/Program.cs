c106813415d356867a3f380bed06fa84758ecc8f
Harry <andr.nitgrafika@yandex.ru>
2023-01-24 23:22:50 +0300

    Added Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень: ");
+int degree = Convert.ToInt32(Console.ReadLine());
+
+int res = Pow(number, degree);
+Console.WriteLine(res);
+
+int Pow(int num, int deg)
+{
+    int result = 1;
+    while (deg > 0)
+    {
+        result *= num;
+        deg--;
+    }
+    return result;
+}
