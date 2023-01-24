a822e8c5b4e17c84c894368ef617c88bbc5c4146
Harry <andr.nitgrafika@yandex.ru>
2023-01-24 23:40:46 +0300

    Added Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = SumDigits(num); 
+Console.WriteLine($"Сумма цифр числа равна: {sum}");
+
+
+int SumDigits(int number)
+{
+    int sum = 0;
+    while(number > 0)
+    {
+        int lastDigit = number % 10;
+        sum += lastDigit;
+        number = number / 10; 
+        if(number < 10)
+        {
+            sum += number;
+            break;
+        }
+    }
+    return sum;
+}
