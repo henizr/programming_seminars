5e5e0879e770132bdd2751ad8cee6187a0fd97bc
Harry <andr.nitgrafika@yandex.ru>
2023-02-01 01:04:28 +0300

    Added Task43

---
+++ Task43/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите b1");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k1");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b2");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k2");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+double x = GetX(b1, k1, b2, k2);
+double y = GetY(k1, b1, x);
+
+string result = $"Точка пересечения двух прямых -> ({x},{y})";
+Console.WriteLine(result);
+
+double GetX(double _b1, double _k1, double _b2, double _k2)
+{
+    return (_b2-_b1)/(_k1-_k2);
+}
+
+double GetY(double _k1, double _b1, double _x)
+{
+    return _k1*_x+_b1;
+}
