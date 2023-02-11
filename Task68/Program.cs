049ee1a265a1ad5f87ba8ec8ff2d403fa94d3330
Harry <andr.nitgrafika@yandex.ru>
2023-02-11 21:01:49 +0300

    Added Task68

---
+++ Task68/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("m: ");
+int M = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("n: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int a = A(M,N);
+
+Console.WriteLine($"m = {M}, n = {N} -> A(m,n) = {a}");
+
+int A(int n, int m)
+{
+    if (n == 0)
+    {
+        return m + 1;
+    }
+    else if(m == 0)
+    {
+       return A(n-1, 1);
+    }
+    else
+    {
+        return A(n-1, A(n, m-1));
+    }
+}
