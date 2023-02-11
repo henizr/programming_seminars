8eb704f187fae509f1694fac71bdfc773212eed8
Harry <andr.nitgrafika@yandex.ru>
2023-02-11 21:01:37 +0300

    Added Task67

---
+++ Task67/Program.cs
@@ -0,0 +1,22 @@
+﻿// 45 -> 12
+Convert(45);
+Console.WriteLine(SumNum(45));
+ 
+
+void Convert(int num)
+{
+    int sum = 0;
+    while(num != 0)
+    {
+        sum += num%10;
+        num /= 10;
+    }
+
+    Console.WriteLine(sum);
+}
+
+int SumNum(int num)
+{
+    if(num == 0) return 0;
+    return SumNum(num/10) + num%10;
+}
