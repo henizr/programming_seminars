18ead4c40f0a315de67c672df588858d956ef20e
Harry <andr.nitgrafika@yandex.ru>
2023-02-11 21:00:54 +0300

    Added Task64

---
+++ Task64/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите значение N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+if (N > 0)
+{
+    Console.Write($"N={N} -> ");
+    ShowNumbers(N);
+}
+else{
+    Console.WriteLine("Число не натуральное");
+}
+
+
+void ShowNumbers(int n)
+{
+    if(n == 0) return;
+    Console.Write($"{n} ");
+    ShowNumbers(n-1);   
+}
